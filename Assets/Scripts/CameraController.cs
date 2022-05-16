d3c304d00cfa94da2a830ca99e249ffc4f3ec0d6
Raghiiboii Baxtor <85328806+raghiiboiibaxtor@users.noreply.github.com>
2022-05-16 19:52:36 +1200

    Camera Follow Update

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,11 +4,6 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-
-    [SerializeField] private float speed;
-    private float currentPosX;
-    private Vector3 velocity = Vector3.zero;
-
     // Follow Player
     [SerializeField] private Transform player;
     // Camera variables to look forward
@@ -21,14 +16,10 @@ public class CameraController : MonoBehaviour
     void Update()
     {
         // Follow Player : Moves camera with player on x axis, leaves y and z axis unchanged.
-        transform.position = new Vector3(player.position.x, transform.position.y, transform.position.z);
+        transform.position = new Vector3(player.position.x + lookAhead, transform.position.y, transform.position.z);
         // Camera will take player's position on x and add/minus aheadDistance to move slightly forward.
-        lookAhead = Mathf.Lerp(lookAhead, (aheadDistance * player.localScale.x), Time.deltaTime* cameraSpeed);
+        lookAhead = Mathf.Lerp(lookAhead, (aheadDistance * player.localScale.x), Time.deltaTime * cameraSpeed);
     }
 
-    public void MoveToNewRoom(Transform _newroom)
-    {
-        currentPosX = _newroom.position.x;
-    }
-
+ 
 }
