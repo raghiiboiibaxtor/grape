c651a2c889a8253b21ce9a7bea7d8044afb04cd4
Raghiiboii Baxtor <85328806+raghiiboiibaxtor@users.noreply.github.com>
2022-05-16 22:09:26 +1200

    HealthBar(Incomplete)

--- Assets/Scripts/Health/Health.cs
+++ Assets/Scripts/Health/Health.cs
@@ -4,15 +4,40 @@ using UnityEngine;
 
 public class Health : MonoBehaviour
 {
+    [SerializeField] private float startingHealth;
+    // Allowing for public access to get, but private access to set
+    public float currentHealth {get; private set;}
+
+
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        currentHealth = startingHealth;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            TakeDamage(01);
+        }
     }
+
+    public void TakeDamage(float _damage)
+    {
+        currentHealth = Mathf.Clamp(currentHealth - _damage, 0, startingHealth);
+
+        if (currentHealth > 0)
+        {
+            ; // Hurt
+        }
+        else
+        {
+            ; // Dead
+        }
+
+
+    }
+
 }
---
+++ Assets/Scripts/Health/HealthBar.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    [SerializeField] private Health playerHealth;
+    [SerializeField] private Image totalHealthBar;
+    [SerializeField] private Image currentHealthBar;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        totalHealthBar.fillAmount = playerHealth.currentHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentHealthBar.fillAmount = playerHealth.currentHealth;
+    }
+}
