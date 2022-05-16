987c22f839ea2728ef2bf08554f1f404c6f507ce
Raghiiboii Baxtor <85328806+raghiiboiibaxtor@users.noreply.github.com>
2022-05-16 19:06:27 +1200

    Camera Move

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -9,14 +9,18 @@ public class CameraController : MonoBehaviour
     private float currentPosX;
     private Vector3 velocity = Vector3.zero;
 
+    // Follow Player
+    [SerializeField] private Transform player;
 
 
     // Update is called once per frame
     void Update()
     {
         // Using SmoothDamp method to smooth moving camera frames. Passing position, new position, velocity & speed as parameters.
-        transform.position = Vector3.SmoothDamp(transform.position, new Vector3(currentPosX, transform.position.y, transform.position.z), ref velocity, speed * Time.deltaTime);
+        //transform.position = Vector3.SmoothDamp(transform.position, new Vector3(currentPosX, transform.position.y, transform.position.z), ref velocity, speed * Time.deltaTime);
 
+        // Follow Player : Moves camera with player on x axis, leaves y and z axis unchanged.
+        transform.position = new Vector3(player.position.x, transform.position.y, transform.position.z);
     }
 
     public void MoveToNewRoom(Transform _newroom)
