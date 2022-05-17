3f6ca6ad5bbc795b6f40458a7bd87f52249d65e2
Raghiiboii Baxtor <85328806+raghiiboiibaxtor@users.noreply.github.com>
2022-05-18 10:12:11 +1200

    Safety Push

--- Assets/Scripts/Enemy_Sideways.cs
+++ Assets/Scripts/Enemy_Sideways.cs
@@ -2,9 +2,51 @@ using UnityEngine;
 
 public class Enemy_Sideways : MonoBehaviour
 {
-
+    [SerializeField] private float movementDistance;
     [SerializeField] private float damage;
-   
+    [SerializeField] private float speed;
+
+    private bool movingLeft;
+    private float leftEgde;
+    private float rightEdge;
+
+
+    private void Awake()
+    {
+        leftEgde = transform.position.x - movementDistance;
+        leftEgde = transform.position.x + movementDistance;
+    }
+
+    private void Update()
+    {
+        // Nested IfElse block to check whether or not the Rock obj is moving left or right.
+        // Different directions will trigger different results.
+        if (movingLeft)
+        {
+            if (transform.position.x > leftEgde)
+            {
+                transform.position = new Vector3(transform.position.x - speed * Time.deltaTime, transform.position.y, transform.position.z);
+            }
+            else
+            {
+                movingLeft = false;
+
+            }
+        }
+        else
+        {
+            if (transform.position.x < rightEdge)
+            {
+                transform.position = new Vector3(transform.position.x + speed * Time.deltaTime, transform.position.y, transform.position.z);
+            }
+            else
+            {
+                movingLeft = true;
+
+            }
+        }
+
+    }
 
     // Method detecting for player collisions 
     private void OnTriggerEnter2D(Collider2D collision)
