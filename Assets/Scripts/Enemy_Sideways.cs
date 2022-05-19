9a23ccbd371e4c0703a9a682cc2768289d7285a5
Raghiiboii Baxtor <85328806+raghiiboiibaxtor@users.noreply.github.com>
2022-05-19 12:12:42 +1200

    Enemy Sideways Movement

--- Assets/Scripts/Enemy_Sideways.cs
+++ Assets/Scripts/Enemy_Sideways.cs
@@ -1,37 +1,30 @@
-using UnityEngine;
+  using UnityEngine;
 
 public class Enemy_Sideways : MonoBehaviour
 {
     [SerializeField] private float movementDistance;
-    [SerializeField] private float damage;
     [SerializeField] private float speed;
-
+    [SerializeField] private float damage;
     private bool movingLeft;
-    private float leftEgde;
+    private float leftEdge;
     private float rightEdge;
-
 
     private void Awake()
     {
-        leftEgde = transform.position.x - movementDistance;
-        leftEgde = transform.position.x + movementDistance;
+        leftEdge = transform.position.x - movementDistance;
+        rightEdge = transform.position.x + movementDistance;
     }
 
     private void Update()
     {
-        // Nested IfElse block to check whether or not the Rock obj is moving left or right.
-        // Different directions will trigger different results.
         if (movingLeft)
         {
-            if (transform.position.x > leftEgde)
+            if (transform.position.x > leftEdge)
             {
                 transform.position = new Vector3(transform.position.x - speed * Time.deltaTime, transform.position.y, transform.position.z);
             }
             else
-            {
                 movingLeft = false;
-
-            }
         }
         else
         {
@@ -40,20 +33,14 @@ public class Enemy_Sideways : MonoBehaviour
                 transform.position = new Vector3(transform.position.x + speed * Time.deltaTime, transform.position.y, transform.position.z);
             }
             else
-            {
                 movingLeft = true;
-
-            }
         }
-
     }
 
-    // Method detecting for player collisions 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.tag == "Player")
         {
-            // Grabbing health component to borrow TakeDamage method to execute enemy damage
             collision.GetComponent<Health>().TakeDamage(damage);
         }
     }
